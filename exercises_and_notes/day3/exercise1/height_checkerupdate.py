4b44ff4751d4b87e3b4a6e8e04a31cec5800b554
zo-88 <67532480+zo-88@users.noreply.github.com>
2023-01-16 12:19:12 +0100

    Delete height_checkerupdate.py

--- exercises_and_notes/day3/exercise1/height_checkerupdate.py
+++
@@ -1,8 +0,0 @@
-#Control flow if/ else statements and conditional operators
-
-print("Welcome to the rollercoaster!")
-height = int(input("What is your height in cm? "))
-if height >= 120:
-  print("You're tall enough to ride the rollacoster")
-else:
-  print("Sorry you can't ride")
