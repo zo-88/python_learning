b6b624d9242b0843b170a36b0fcd070d7ae50be2
zoey lampkin <zoeylampkin@zoeys-MacBook-Air.local>
2023-01-16 20:43:18 +0100

    final version of rollercoaster generator

--- exercises_and_notes/day3/exercise1/rollercoaster_check.py
+++ exercises_and_notes/day3/exercise1/rollercoaster_check.py
@@ -20,9 +20,9 @@ if height >= 120:
   wants_photo = input("Would you like a photo? Y/N: ").lower()
   if wants_photo == 'y':
       bill += 3
-      print(f"Your total ticket is {bill} with a photo")
-  else:
-      print(f"Your total ticket is {bill}")
+  
+  print(f"Your final bill is ${bill}")
+
  
       
       
