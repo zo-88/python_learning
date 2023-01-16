44977a76172f7eea14bb16c3403daaa3cbcc938b
zoey lampkin <zoeylampkin@zoeys-MacBook-Air.local>
2023-01-16 17:40:24 +0100

    updated rollercoaster program to final product and added final control flow

--- exercises_and_notes/day3/exercise1/rollercoaster_check.py
+++ exercises_and_notes/day3/exercise1/rollercoaster_check.py
@@ -4,5 +4,12 @@ print("Welcome to the rollercoaster!")
 height = int(input("What is your height in cm? "))
 if height >= 120:
   print("Can ride")
+  age = int(input("what is your age? "))
+  if age < 12:
+      print("Your ticket is $5")
+  elif age <= 18:
+      print("Your ticket is $7")
+  else:
+      print("Your ticket is $12")
 else:
   print("can't ride")
