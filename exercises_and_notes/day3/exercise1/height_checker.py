09f72719001d736a936a690522ff80beea763426
zo-88 <67532480+zo-88@users.noreply.github.com>
2023-01-16 12:09:32 +0100

    Create height_checker.py

---
+++ exercises_and_notes/day3/exercise1/height_checker.py
@@ -0,0 +1,8 @@
+#Control flow if/ else statements and conditional operators
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if height > 120:
+  print("Can ride")
+else:
+  print("can't ride")
