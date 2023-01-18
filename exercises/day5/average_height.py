fe504a3aa335dde5a9ca9e516d82d1cd21adbb99
zoey lampkin <zoeylampkin@zoeys-MacBook-Air.local>
2023-01-18 11:54:49 +0100

    find average height

---
+++ exercises/day5/average_height.py
@@ -0,0 +1,8 @@
+# use a for loop to calculate the average height of a group of students
+
+student_heights = [180, 124, 165, 173, 189, 169, 146]
+
+average_height = round(sum(student_heights)/len(student_heights))
+
+print(f"The average height is {average_height}cm")
+
---
+++ exercises/day5/average_height_loops.py
@@ -0,0 +1,21 @@
+# Use a for loop to calculate the average height of a group of students.
+student_heights = input("Input a list a student heights: ").split()
+
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+               
+
+print(student_heights)
+
+num_of_students = 0
+
+height_total = 0
+
+for height in student_heights:
+    height_total += height
+    num_of_students += 1
+    
+    
+average_height = round(height_total/num_of_students)
+
+print(f"The average height is {average_height}")
