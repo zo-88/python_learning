73002475cff3b3d707e7f9ca731e002a601401de
zoey lampkin <zoeylampkin@zoeys-MacBook-Air.local>
2023-01-18 12:06:47 +0100

    find highest score

---
+++ exercises/day5/highest_score_loops.py
@@ -0,0 +1,9 @@
+
+student_scores = [78, 65, 89, 86, 55, 91, 64, 89]
+
+for score in student_scores:
+    highest_score = 0
+    if highest_score < score:
+        highest_score = score
+        
+print(f"The highest score in the class is: {highest_score}")
