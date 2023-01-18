4f5f5b6dc15b593b04bdf65154ac0ee096863f6e
zoey lampkin <zoeylampkin@zoeys-MacBook-Air.local>
2023-01-18 12:14:38 +0100

    updated highest score finder

--- exercises/day5/highest_score_loops.py
+++ exercises/day5/highest_score_loops.py
@@ -1,5 +1,13 @@
 
-student_scores = [78, 65, 89, 86, 55, 91, 64, 89]
+#student_scores = [78, 65, 89, 86, 55, 91, 64, 89]
+
+
+student_scores = input("Enter student scores: ").split()
+
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+    
+               
 
 for score in student_scores:
     highest_score = 0
@@ -7,3 +15,10 @@ for score in student_scores:
         highest_score = score
         
 print(f"The highest score in the class is: {highest_score}")
+
+
+# using max function()
+
+score_max = max(student_scores)
+print(f"This is the maximum score: {score_max}")
+print(f"This is the lowerest score: {min(student_scores)}")
