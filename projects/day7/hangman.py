a53afc3189ef5f7ff41641cd581e2929edca8d6d
zoey lampkin <zoeylampkin@zoeys-MacBook-Air.local>
2023-01-19 09:22:15 +0100

    update

--- projects/day7/hangman.py
+++ projects/day7/hangman.py
@@ -1,19 +1,39 @@
-word_list = ["ardvark", "baboon", "camel"]
+
 
 #step 1
 import random
-
-#TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
-print(chosen_word)
-#TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-guess = input("Guess a letter: ").lower()
+word_length = len(chosen_word)
 
-#TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+print(f"chosen word is {chosen_word}")
 
-for letter in chosen_word:
-    if letter == guess:
-        print("right")
-    else:
-        print("wrong")
+display = []
+for i in range(word_length):
+    display += "_"
+    
+print(display)
+ 
+
+end_of_game = False
+
+while not end_of_game:
+    guess = input("Guess a letter: ").lower()
+    
+    for position in range(word_length):
+        letter = chosen_word[position]
+        if letter == guess:
+            display[position] = guess
+    print(display)
+        
+    if '_' not in display:
+        end_of_game = True
+        print("You win")
+        
+        
+ 
+
+
+
+
     
